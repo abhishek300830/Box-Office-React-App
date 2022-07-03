769f98fbc59b0d39e91457b7698d720643aff540
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-07-03 10:25:30 +0530

    added Loading and error messages

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -6,14 +6,37 @@ const Shows = () => {
    //    const params = useParams();
    const { id } = useParams();
    const [show, setShow] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
-      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result => {
-         setShow(result);
-      });
+      let isMounted = true;
+      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+         .then(result => {
+            if (isMounted) {
+               setShow(result);
+               setIsLoading(false);
+            }
+         })
+         .catch(err => {
+            if (isMounted) {
+               setError(err.message);
+               setIsLoading(false);
+            }
+         });
+      return () => {
+         isMounted = false;
+      };
    }, [id]);
 
    console.log('show', show);
+
+   if (isLoading) {
+      return <div>The Data is Loading</div>;
+   }
+   if (error) {
+      return <div>Error Occured : {error}</div>;
+   }
 
    return <div>This is hunks {id}</div>;
 };
