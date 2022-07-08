a6e0c8042acfdb3f9f875b522bfe0b27fd7b9ea9
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-07-08 11:06:24 +0530

    replace useState with useReducer

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -1,27 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+   switch (action.type) {
+      case 'FETCH_SUCCESS': {
+         return { isLoading: false, show: action.show, error: null };
+      }
+      case 'FETCH_FAILED': {
+         return { ...prevState, isLoading: false, error: action.error };
+      }
+      default:
+         return prevState;
+   }
+};
+
+const initialState = {
+   show: null,
+   isLoading: true,
+   error: null,
+};
 
 const Shows = () => {
    //    const params = useParams();
    const { id } = useParams();
-   const [show, setShow] = useState(null);
-   const [isLoading, setIsLoading] = useState(true);
-   const [error, setError] = useState(null);
+
+   const [state, dispatch] = useReducer(reducer, initialState);
+   const { isLoading, error, show } = state;
+   // const [show, setShow] = useState(null);
+   // const [isLoading, setIsLoading] = useState(true);
+   // const [error, setError] = useState(null);
 
    useEffect(() => {
       let isMounted = true;
       apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
          .then(result => {
             if (isMounted) {
-               setShow(result);
-               setIsLoading(false);
+               dispatch({ type: 'FETCH_SUCCESS', show: result });
+               // setShow(result);
+               // setIsLoading(false);
             }
          })
          .catch(err => {
             if (isMounted) {
-               setError(err.message);
-               setIsLoading(false);
+               dispatch({ type: 'FETCH_FAILED', error: err.message });
+               // setError(err.message);
+               // setIsLoading(false);
             }
          });
       return () => {
