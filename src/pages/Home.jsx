7626221bf0f91f032112490aadddf18e95a43fd5
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-07-10 11:33:27 +0530

    create two new hooks: useLastQuery and useShow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import Title from '../components/Title';
 import { apiGet } from '../misc/config';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-   const [input, setInput] = useState('');
+   const [input, setInput] = useLastQuery();
    const [result, setResult] = useState(null);
    const [searchOption, setSearchOption] = useState('shows');
 
--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -1,62 +1,16 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
+import { useShow } from '../misc/custom-hooks';
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
-
-const reducer = (prevState, action) => {
-   switch (action.type) {
-      case 'FETCH_SUCCESS': {
-         return { isLoading: false, show: action.show, error: null };
-      }
-      case 'FETCH_FAILED': {
-         return { ...prevState, isLoading: false, error: action.error };
-      }
-      default:
-         return prevState;
-   }
-};
-
-const initialState = {
-   show: null,
-   isLoading: true,
-   error: null,
-};
 
 const Shows = () => {
    //    const params = useParams();
    const { id } = useParams();
-
-   const [state, dispatch] = useReducer(reducer, initialState);
-   const { isLoading, error, show } = state;
-   // const [show, setShow] = useState(null);
-   // const [isLoading, setIsLoading] = useState(true);
-   // const [error, setError] = useState(null);
-
-   useEffect(() => {
-      let isMounted = true;
-      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-         .then(result => {
-            if (isMounted) {
-               dispatch({ type: 'FETCH_SUCCESS', show: result });
-               // setShow(result);
-               // setIsLoading(false);
-            }
-         })
-         .catch(err => {
-            if (isMounted) {
-               dispatch({ type: 'FETCH_FAILED', error: err.message });
-               // setError(err.message);
-               // setIsLoading(false);
-            }
-         });
-      return () => {
-         isMounted = false;
-      };
-   }, [id]);
+   const { isLoading, error, show } = useShow(id);
 
    // console.log('show', show);
 
