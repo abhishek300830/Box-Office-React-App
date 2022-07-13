9b4954ae1f46448ae753e4c1dc2040aac3399de2
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2022-07-13 08:13:20 +0530

    Making it PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from '/src/serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,4 +16,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
+serviceWorkerRegistration.register();
